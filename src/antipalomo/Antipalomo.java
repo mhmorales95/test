2540a815dad6b4c9ed835781ca141dac20a63303
Mauricio <Mauricio@DESKTOP-GDE8RGT>
2019-05-08 20:27:11 -0300

    Diseño de Menu

--- src/antipalomo/Antipalomo.java
+++ src/antipalomo/Antipalomo.java
@@ -15,7 +15,7 @@ public class Antipalomo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("palomito");
     }
     
 }
